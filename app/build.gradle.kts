249095bba4a67f7f043f5c689393e92878fedaa4
MaraiNicolas <nicolasmarai@hotmail.com>
2025-10-26 13:49:16 -0300

    Agrego Coil --v 3.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,11 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    //Coil --v 3.2.0
+    implementation(libs.coil.compose)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
