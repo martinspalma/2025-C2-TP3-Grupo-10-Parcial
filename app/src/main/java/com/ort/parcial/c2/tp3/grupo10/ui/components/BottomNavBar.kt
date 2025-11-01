f526a84d4facded1a6b1f92311de5b00f77a661a
Jonathan Vitangeli <jonathanfacundo@hotmail.com>
2025-10-31 21:32:12 -0300

    mejoras en el navbar(falta ajustar los espacios y altura entre los iconos)

--- app/src/main/java/com/ort/parcial/c2/tp3/grupo10/ui/components/BottomNavBar.kt
+++ app/src/main/java/com/ort/parcial/c2/tp3/grupo10/ui/components/BottomNavBar.kt
@@ -1,13 +1,17 @@
 package com.ort.parcial.c2.tp3.grupo10.ui.components
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,11 +22,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.ort.parcial.c2.tp3.grupo10.R
+import com.ort.parcial.c2.tp3.grupo10.ui.theme.Caribbean
+import com.ort.parcial.c2.tp3.grupo10.ui.theme.LightGreen
+import com.ort.parcial.c2.tp3.grupo10.ui.theme.Void
 
 // 1. Se define una data class para que el código sea más claro y robusto
 data class BottomNavItem(
@@ -42,22 +51,44 @@ val bottomNavItems = listOf(
 @Composable
 fun BottomNavBar(
     selected: Int,
-    onSelect: (Int) -> Unit
+    onSelect: (Int) -> Unit,
+    cornerRadius: Dp = 34.dp
 ) {
     NavigationBar(
-        modifier = Modifier.clip(RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp))
+        modifier = Modifier.clip(RoundedCornerShape(topStart = 60.dp, topEnd = 60.dp)),
+        containerColor = LightGreen
     ) {
         bottomNavItems.forEachIndexed { index, item ->
             NavigationBarItem(
                 selected = selected == index,
                 onClick = { onSelect(index) },
                 icon = {
-                    Icon(
-                        imageVector = ImageVector.vectorResource(id = item.iconRes),
-                        contentDescription = item.label
-                    )
+                    Box(contentAlignment = Alignment.Center) {
+                        // (NUEVO) Dibujamos nuestro propio indicador si está seleccionado
+                        if (selected == index) {
+                            Box(
+                                modifier = Modifier
+                                    .size(size = 50.dp)
+                                    .background(
+                                        color = Caribbean,
+                                        // Un rectangulo con puntas redondeadas
+                                        shape = RoundedCornerShape(20.dp)
+                                    )
+                            )
+                        }
+                        Icon(
+                            imageVector = ImageVector.vectorResource(id = item.iconRes),
+                            contentDescription = item.label
+                        )
+                    }
                 },
-                label = null
+                label = null,
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = Void,
+                    unselectedIconColor = Void,
+                    // (NUEVO) Hacemos el indicador por defecto transparente
+                    indicatorColor = Color.Transparent
+                )
             )
         }
     }
