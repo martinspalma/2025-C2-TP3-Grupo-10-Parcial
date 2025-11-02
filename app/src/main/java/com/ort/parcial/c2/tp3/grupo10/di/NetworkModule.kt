4cd64e74f28f2d0d07ab577d96bfd1dfe5808aa4
MaraiNicolas <nicolasmarai@hotmail.com>
2025-11-02 18:40:34 -0300

    Agrego string en Recursos pantalla Login y Register. Agrego retrofit para pantalla Login

---
+++ app/src/main/java/com/ort/parcial/c2/tp3/grupo10/di/NetworkModule.kt
@@ -0,0 +1,32 @@
+package com.ort.parcial.c2.tp3.grupo10.di
+
+import com.ort.parcial.c2.tp3.grupo10.shared.infraestructura.authservice.AuthApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "https://d9811bf4-5e67-4a8c-bdcf-603cbbfc0275.mock.pstmn.io/"
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthApi(retrofit: Retrofit): AuthApi {
+        return retrofit.create(AuthApi::class.java)
+    }
+}
