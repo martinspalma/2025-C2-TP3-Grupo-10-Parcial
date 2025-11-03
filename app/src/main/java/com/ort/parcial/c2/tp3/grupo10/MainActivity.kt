c27337e47c4567fac2f6151fde7355ae459f1770
Martin <martinpalma@yahoo.com>
2025-11-02 23:36:20 -0300

    perfil, primer pantalla y componentes para pantalla de confirmacion

--- app/src/main/java/com/ort/parcial/c2/tp3/grupo10/MainActivity.kt
+++ app/src/main/java/com/ort/parcial/c2/tp3/grupo10/MainActivity.kt
@@ -4,20 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.ort.parcial.c2.tp3.grupo10.ui.screens.GenericConfirmationScreen
 import com.ort.parcial.c2.tp3.grupo10.ui.screens.forgotpassword.ForgotPasswordScreen
 import com.ort.parcial.c2.tp3.grupo10.ui.screens.auth.LoginScreen
 import com.ort.parcial.c2.tp3.grupo10.ui.screens.auth.RegisterScreen
 import com.ort.parcial.c2.tp3.grupo10.ui.screens.WelcomeScreen
 import com.ort.parcial.c2.tp3.grupo10.ui.screens.forgotpassword.NewPasswordScreen
 import com.ort.parcial.c2.tp3.grupo10.ui.screens.forgotpassword.SecurityPinScreen
-import com.ort.parcial.c2.tp3.grupo10.ui.screens.forgotpassword.SuccessConfirmationScreen
 import com.ort.parcial.c2.tp3.grupo10.ui.screens.home.HomeScreen
 import com.ort.parcial.c2.tp3.grupo10.ui.theme.MyApplicationTheme
 import com.ort.parcial.c2.tp3.grupo10.ui.screens.onboarding.OnboardingScreen1
 import com.ort.parcial.c2.tp3.grupo10.ui.screens.onboarding.OnboardingScreen2
+import com.ort.parcial.c2.tp3.grupo10.ui.screens.profile.ProfileScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -30,12 +32,13 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 NavHost(
                     navController = navController,
-                    startDestination = "forgotPassword"
+                    startDestination = "profile"
                 ) {
                     composable("forgotPassword") { ForgotPasswordScreen(navController = navController) }
                     composable("securityPin") { SecurityPinScreen(navController = navController) }
                     composable("newPassword") { NewPasswordScreen(navController = navController) }
-                    composable("successConfirmation") { SuccessConfirmationScreen(navController = navController) }
+                    //composable("successConfirmation") { SuccessConfirmationScreen(navController = navController) }
+                    composable("successConfirmation") {GenericConfirmationScreen(navController = navController, message = stringResource(R.string.success_message), destinationRoute = "login")}
                     composable("welcome") { WelcomeScreen(navController = navController) }
                     composable("login") { LoginScreen(navController = navController) }
                     composable("onboarding1") { OnboardingScreen1(navController = navController) }
@@ -48,10 +51,20 @@ class MainActivity : ComponentActivity() {
                     }
                     composable("register") { RegisterScreen(navController = navController) }
                     composable("home") { HomeScreen(navController) }
+
+                    // RUTAS PROFILE
+                    // NUEVAS RUTAS PARA FINGERPRINT Y PIN
+
+                    composable("profile") {ProfileScreen(navController = navController)}
+                    composable("fingerprintChangedSuccess") {GenericConfirmationScreen(navController = navController, message = stringResource(R.string.fingerprint_changed_success), destinationRoute = "login")}
+                    composable("pinChangedSuccess") {GenericConfirmationScreen(navController = navController, message = stringResource(R.string.pin_changed_success), destinationRoute = "login") }
+                    composable("fingerprintDeletedSuccess") {GenericConfirmationScreen( navController = navController, message = stringResource(R.string.fingerprint_deleted_success), destinationRoute = "login")}
+
+
                     // Reset password flow
-                    composable("security_pin") { SecurityPinScreen(navController = navController) }
-                    composable("new_password") { NewPasswordScreen(navController = navController) }
-                    composable("password_changed") { SuccessConfirmationScreen(navController = navController) }
+                    //composable("security_pin") { SecurityPinScreen(navController = navController) }
+                    //composable("new_password") { NewPasswordScreen(navController = navController) }
+                    //composable("password_changed") { SuccessConfirmationScreen(navController = navController) }
                 }
             }
         }
