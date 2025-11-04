f778aa5b4ebd902a538a596e659b5998ef815a83
Gabriel Birman <estebanbirman2002@gmail.com>
2025-11-04 13:22:54 -0300

    Solucion error SplashScreen

--- app/src/main/java/com/ort/parcial/c2/tp3/grupo10/MainActivity.kt
+++ app/src/main/java/com/ort/parcial/c2/tp3/grupo10/MainActivity.kt
@@ -1,41 +1,34 @@
 package com.ort.parcial.c2.tp3.grupo10
 
 import android.os.Bundle
-import com.ort.parcial.c2.tp3.grupo10.ui.screens.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.ui.res.stringResource
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.ort.parcial.c2.tp3.grupo10.ui.screens.splash.SplashScreen
-
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.ort.parcial.c2.tp3.grupo10.ui.screens.GenericConfirmationScreen
-import com.ort.parcial.c2.tp3.grupo10.ui.screens.forgotpassword.ForgotPasswordScreen
+import com.ort.parcial.c2.tp3.grupo10.ui.screens.WelcomeScreen
 import com.ort.parcial.c2.tp3.grupo10.ui.screens.auth.LoginScreen
 import com.ort.parcial.c2.tp3.grupo10.ui.screens.auth.RegisterScreen
-import com.ort.parcial.c2.tp3.grupo10.ui.screens.WelcomeScreen
+import com.ort.parcial.c2.tp3.grupo10.ui.screens.categories.CategoriesScreen
+import com.ort.parcial.c2.tp3.grupo10.ui.screens.expenses.AddExpenseScreen
+import com.ort.parcial.c2.tp3.grupo10.ui.screens.expenses.ExpensesScreen
+import com.ort.parcial.c2.tp3.grupo10.ui.screens.forgotpassword.ForgotPasswordScreen
 import com.ort.parcial.c2.tp3.grupo10.ui.screens.forgotpassword.NewPasswordScreen
 import com.ort.parcial.c2.tp3.grupo10.ui.screens.forgotpassword.SecurityPinScreen
+import com.ort.parcial.c2.tp3.grupo10.ui.screens.forgotpassword.SuccessConfirmationScreen
 import com.ort.parcial.c2.tp3.grupo10.ui.screens.home.HomeScreen
-import com.ort.parcial.c2.tp3.grupo10.ui.theme.MyApplicationTheme
 import com.ort.parcial.c2.tp3.grupo10.ui.screens.onboarding.OnboardingScreen1
 import com.ort.parcial.c2.tp3.grupo10.ui.screens.onboarding.OnboardingScreen2
 import com.ort.parcial.c2.tp3.grupo10.ui.screens.profile.ProfileScreen
-import com.ort.parcial.c2.tp3.grupo10.ui.screens.categories.CategoriesScreen
-import com.ort.parcial.c2.tp3.grupo10.ui.screens.expenses.ExpensesScreen
-import androidx.navigation.NavType
-import androidx.navigation.navArgument
-import com.ort.parcial.c2.tp3.grupo10.ui.auth.AuthViewModel
-import dagger.hilt.android.AndroidEntryPoint
-import com.ort.parcial.c2.tp3.grupo10.ui.screens.expenses.AddExpenseScreen
-import com.ort.parcial.c2.tp3.grupo10.ui.screens.forgotpassword.SuccessConfirmationScreen
+import com.ort.parcial.c2.tp3.grupo10.ui.screens.splash.SplashScreen // Corrected import
 import com.ort.parcial.c2.tp3.grupo10.ui.screens.transactions.TransactionsScreen
-
-
-
+import com.ort.parcial.c2.tp3.grupo10.ui.theme.MyApplicationTheme
+import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -45,12 +38,23 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 val navController = rememberNavController()
-                NavHost(
-                    navController = navController,
-                    startDestination = "welcome"
-                ) {
-                    composable("splash") { SplashScreen(navController = navController) }
+               NavHost(
+    navController = navController,
+    startDestination = "splash" 
+)
 
+ {
+                    composable("splash") { SplashScreen(navController = navController) }
+                    composable("welcome") { WelcomeScreen(navController = navController) }
+                    composable("login") { LoginScreen(navController = navController) }
+                    composable("register") { RegisterScreen(navController = navController) }
+                    composable("home") { HomeScreen(navController) }
+
+                    // Onboarding
+                    composable("onboarding1") { OnboardingScreen1(navController = navController) }
+                    composable("onboarding2") { OnboardingScreen2(navController = navController) }
+
+                    // Categories & Expenses
                     composable("categories") { CategoriesScreen(navController = navController) }
                     composable(
                         route = "expenses/{categoryName}",
@@ -64,37 +68,22 @@ class MainActivity : ComponentActivity() {
                             navController = navController
                         )
                     }
+                    composable("add_expense") { AddExpenseScreen(navController = navController) }
 
+                    // Forgot Password Flow
                     composable("forgotPassword") { ForgotPasswordScreen(navController = navController) }
                     composable("securityPin") { SecurityPinScreen(navController = navController) }
                     composable("newPassword") { NewPasswordScreen(navController = navController) }
-                    composable("successConfirmation") { SuccessConfirmationScreen(navController = navController) }
-                    composable(route = "splash") { SplashScreen(navController) }
 
-                    //composable("successConfirmation") { SuccessConfirmationScreen(navController = navController) }
+                    // Generic Confirmation Screens
                     composable("successConfirmation") {GenericConfirmationScreen(navController = navController, message = stringResource(R.string.success_message), destinationRoute = "login")}
-                    composable("welcome") { WelcomeScreen(navController = navController) }
-                    composable("login") { LoginScreen(navController = navController) }
-                    composable("onboarding1") { OnboardingScreen1(navController = navController) }
-                    composable("onboarding2") { OnboardingScreen2(navController = navController) }
-                    composable("register") { RegisterScreen(navController = navController) }
-                    composable("home") { HomeScreen(navController) }
-
-                    // RUTAS PROFILE
-                    // NUEVAS RUTAS PARA FINGERPRINT Y PIN
-
-                    composable("profile") {ProfileScreen(navController = navController)}
                     composable("fingerprintChangedSuccess") {GenericConfirmationScreen(navController = navController, message = stringResource(R.string.fingerprint_changed_success), destinationRoute = "login")}
                     composable("pinChangedSuccess") {GenericConfirmationScreen(navController = navController, message = stringResource(R.string.pin_changed_success), destinationRoute = "login") }
                     composable("fingerprintDeletedSuccess") {GenericConfirmationScreen( navController = navController, message = stringResource(R.string.fingerprint_deleted_success), destinationRoute = "login")}
 
+                    // Profile & Transactions
+                    composable("profile") {ProfileScreen(navController = navController)}
                     composable("transactions") { TransactionsScreen(navController) }
-                    composable("add_expense") { AddExpenseScreen(navController = navController) }
-
-                    // Reset password flow
-                    //composable("security_pin") { SecurityPinScreen(navController = navController) }
-                    //composable("new_password") { NewPasswordScreen(navController = navController) }
-                    //composable("password_changed") { SuccessConfirmationScreen(navController = navController) }
                 }
             }
         }
